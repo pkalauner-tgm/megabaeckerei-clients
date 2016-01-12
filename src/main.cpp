cae8167da7b0735f85db88b46a053eac254607e0
Paul Kalauner <pkalauner@student.tgm.ac.at>
2016-01-12 10:37:44 +0100

    added welcome message

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "SocketHandler.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 int main()
 {
+    cout << "Megabaeckerei-Watertank" << endl;
+    cout << "-----------------------" << endl;
+    cout << endl;
+
     WaterTank wt = WaterTank();
     SocketHandler* sh = new SocketHandler(&wt, "127.0.0.1", 12345);
     wt.setSocketHandler(sh);
